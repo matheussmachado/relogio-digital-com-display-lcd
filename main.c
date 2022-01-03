34c7dc718a31410f5c8c3dbff596351c975038dc
matheus machado <machadom@outlook.com.br>
2022-01-03 00:17:50 -0300

    definindo os modos de operação e desenvolvido o efeito e blink do cursor no modo alteração

--- main.c
+++ main.c
@@ -1,22 +1,37 @@
 #define F_CPU 16000000
+#define SECAO_HORA 0
+#define SECAO_MINUTO 1
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h> //pois é dependencia para a lib LCD
 #include <stdio.h>
 #include "LCD.h"
 
-// PROTÓTIPO DE MACROS PARA INTERRUPÇÕES
-ISR(TIMER1_OVF_vect);
+/*
+ DEFINIÇÕES:
+ * HÁ 3 MODOS/STATUS: EXIBIÇÃO, AJUSTE DE HORÁRIO, AJUSTE DE ALARME
+ * PB4 -> ALTERA O MODO
+ * PC2 -> NOS AJUSTES: ALTERA O "CURSOR" ENTRE HORA E MINUTO
+ * PC4 -> NOS AJUSTES: INCREMENTA A VARIÁVEL EM QUE ESTÁ APONTADO O "CURSOR"
+ */
 
 // MACROS PARA TRATAMENTO DE BITS NOS REGISTRADORES
 #define test_bit(y, bit)    (y & (1 << bit))
+
+// PROTÓTIPOS
+ISR(TIMER1_OVF_vect);
+ISR(PCINT0_vect);
+void executar_modo(int modo);
 
 // VARIÁVEIS GLOBAIS
 volatile unsigned int hora = 0;
 volatile unsigned int minuto = 0;
 volatile unsigned int segundo = 0;
 volatile char buffer[9] = "        ";
-
+volatile unsigned int modo = 0;
+volatile unsigned int exibir_numero = 0;
+volatile unsigned int cursor = SECAO_HORA;
+char modos[][13] = {"HORARIO     ", "ALT. ALARME ", "ALT. HORARIO"};
 
 int main(void) {    
   // DISPLAY LCD
@@ -24,10 +39,14 @@ int main(void) {
   PORTD = 0x00;
   
   // PUSH BUTTONS
-  DDRC = 0b00000000;
+  DDRC = 0x00;
   PORTC = 0b00001100; // habilitando pull up dos push buttons
+  DDRB = 0x00;
+  PORTB = 0b00010000;
   
-  // INTERRUPÇÕES  
+  // INTERRUPÇÕES 
+  PCICR = 0b00000001; // habilitando interrupção do PORTB
+  PCMSK0 = 0b00010000; // habilitando interrupção do pino PB4
   TCCR1B = 0b00000101; // prescaler 1024
   TIMSK1 = 0b00000001; // habilitando interrupção do timer por overflow
   TCNT1 = 49911; // contagem para overflow em 1 seg.
@@ -41,10 +60,8 @@ int main(void) {
 }
 
 ISR(TIMER1_OVF_vect) {
-  TCNT1 = 49911;
-  sprintf(buffer, "%.2d:%.2d:%.2d", hora, minuto, segundo);
-  cmd_LCD(0x80, 0);
-  escreve_LCD(buffer);
+  TCNT1 = 49911;  
+  executar_modo(modo);
   segundo++;
   if (segundo == 60) {
     segundo = 0;
@@ -57,4 +74,38 @@ ISR(TIMER1_OVF_vect) {
       }
     }    
   }  
+}
+
+ISR(PCINT0_vect) {
+  if (!test_bit(PINB, PB4)) {
+    modo++;
+    if (modo == 3) modo = 0;
+    
+  }
+}
+
+void executar_modo(int modo) {
+  cmd_LCD(0x80, 0);
+  escreve_LCD(modos[modo]);
+  cmd_LCD(0xC0, 0);
+  switch (modo) {
+    case 0:
+      sprintf(buffer, "%.2d:%.2d:%.2d", hora, minuto, segundo);
+      break;
+    case 1:
+      if (!exibir_numero) {
+        if (cursor == SECAO_HORA) {
+          sprintf(buffer, "  :%.2d:%.2d", minuto, segundo);
+        }
+        else if (cursor == SECAO_MINUTO) {
+          sprintf(buffer, "%.2d:  :%.2d", hora, minuto, segundo);
+        }
+      }
+      else {              
+        sprintf(buffer, "%.2d:%.2d:%.2d", hora, minuto, segundo);                               
+      }
+      exibir_numero = !exibir_numero;
+      break;
+  } 
+  escreve_LCD(buffer);
 }
