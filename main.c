a6de0ff150d996425f79fff6960a5fac5f4bf4cf
matheus machado <machadom@outlook.com.br>
2022-01-04 12:35:06 -0300

    desenvolvido controle contínuo de incremento de valores pelo usuário e organizando o código

--- main.c
+++ main.c
@@ -1,60 +1,54 @@
+/*
+ LEIA O README
+ */
+
 #define F_CPU 16000000
 #define SECAO_HORA 0
 #define SECAO_MINUTO 1
 #define EXIBIR_HORARIO 0
 #define ALTERAR_ALARME 1
 #define ALTERAR_HORARIO 2
+#define FLAG_EXIBICAO 20 // para overflow de 10 us -> exibição será atualizada a cada {FLAG_EXIBICAO} ms
+#define FLAG_INCREMENTO 15 // para overflow de 10 us -> com botão pressionado, será incrementado a 
+                           // cada {FLAG_INCREMENTO} ms
+#define test_bit(y, bit)    (y & (1 << bit)) // macro para teste de bit em um registrador
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h> //pois é dependencia para a lib LCD
+#include <util/delay.h> //pois é dependencia para a lib em LCD.h
 #include <stdio.h>
 #include "LCD.h"
 
-/*
- DEFINIÇÕES:
- * HÁ 3 MODOS/STATUS: EXIBIÇÃO, AJUSTE DE HORÁRIO, AJUSTE DE ALARME
- * PB4 -> ALTERA O MODO
- * PC2 -> NOS AJUSTES: ALTERA O "CURSOR" ENTRE HORA E MINUTO
- * PC4 -> NOS AJUSTES: INCREMENTA A VARIÁVEL EM QUE ESTÁ APONTADO O "CURSOR"
- */
-
-/*
- TAREFAS:
- * - IMPLEMENTAR O ALARME
- */
-
-// MACROS PARA TRATAMENTO DE BITS NOS REGISTRADORES
-#define test_bit(y, bit)    (y & (1 << bit))
-#define set_bit(y, bit)     (y |= (1 << bit))
-#define clear_bit(y, bit)   (y &= ~(1 << bit))
 
 // PROTÓTIPOS
 ISR(TIMER0_OVF_vect);
 ISR(TIMER1_OVF_vect);
 ISR(PCINT0_vect);
 ISR(PCINT1_vect);
-void exibir_modo(int modo);
 void blink_cursor();
 void acionar_alarme();
 void desacionar_alarme();
-void tratamento_de_horario(volatile unsigned int *arg_hora, volatile unsigned int *arg_minuto);
+void exibir_modo(int modo);
+void incrementar_horario();
 void atualizar_exibicao(volatile unsigned int arg_hora, volatile unsigned int arg_minuto);
+void tratamento_de_horario(volatile unsigned int *arg_hora, volatile unsigned int *arg_minuto);
 
 // VARIÁVEIS GLOBAIS
 volatile unsigned int hora = 0, minuto = 0, segundo = 0;
 volatile unsigned int hora_exib = 0, minuto_exib = 0;
 volatile unsigned int hora_alarme = 0, minuto_alarme = 0;
 volatile unsigned int hora_tmp, minuto_tmp;
-volatile char buffer[9] = "        ";
 volatile unsigned int modo = 0;
 volatile unsigned int exibir_numero = 0;
 volatile unsigned char cursor = SECAO_HORA;
 volatile unsigned int contador_timer0 = 0;
+volatile unsigned int contador_incremento = 0;
+volatile char buffer[9] = "        ";
 char modos[][13] = {"HORARIO     ", "ALT. ALARME ", "ALT. HORARIO"};
 
 
 int main(void) {    
-  // DISPLAY LCD
+  // DISPLAY LCD E ATUADOR DO ALARME
   DDRD = 0b11111000;
   PORTD = 0x00;
   
@@ -78,24 +72,35 @@ int main(void) {
   
   // LCD    
   inic_LCD_4bits();
-  hora = 23;
-  hora_alarme = 23;
-  minuto_alarme = 55;
-  minuto = 55;
+  
+  hora = 16;
+  minuto = 00;
+  hora_alarme = 13;
+  minuto_alarme = 30;
+  
   
   while (1) {
     if (hora_alarme == hora && minuto_alarme == minuto) {
-      PORTD |= (1 << 3);
+      acionar_alarme();
+      
     }
-    else PORTD &= ~(1 << 3);
+    else desacionar_alarme(); 
   }
 }
 
+// TRTAMENTO DE INTERRUPÇÕES
 ISR(TIMER0_OVF_vect) {
-  // DESCRIÇÃO: CONTROLE DA TAXA DE EXIBIÇÃO DO MODO -> A CADA 0,5 SEGUNDOS
+  // DESCRIÇÃO: CONTROLE DA TAXA DE EXIBIÇÃO DO MODO
   TCNT0 = 100;
   contador_timer0++;
-  if (contador_timer0 == 50) {
+  if (!test_bit(PINC, PC3)) {
+    contador_incremento++;
+    if (contador_incremento == FLAG_INCREMENTO) {
+      contador_incremento = 0;
+      incrementar_horario();
+    }
+  }
+  if (contador_timer0 == FLAG_EXIBICAO) {
     contador_timer0 = 0; 
     exibir_modo(modo);
   }
@@ -128,21 +133,26 @@ ISR(PCINT0_vect) {
 }
 
 ISR(PCINT1_vect) {
-  // DESCRIÇÃO: CONTROLE DO MAPEAMETO DO CURSOR PISCANTE E DO INCREMENTO DO
-  //            NÚMERO DA SEÇÃO CUJO CURSOR ESTÁ APONTANDO
+  // DESCRIÇÃO: CONTROLE DO MAPEAMETO DO CURSOR PISCANTE E DA FINALIZAÇÃO DO
+  //            INCREMENTO DO NÚMERO DA SEÇÃO CUJO CURSOR ESTÁ APONTANDO
   if (!test_bit(PINC, PC2)) {
     cursor = !cursor; // seu mapeamento é entre os números 0 e 1      
+  }  
+  if (test_bit(PINC, PC3)) {
+    contador_incremento = 0;
   }
-  if (!test_bit(PINC, PC3)) {
-    hora_tmp = minuto_tmp = 0;
-    if (cursor == SECAO_HORA) hora_tmp++;
-    else minuto_tmp++;
-    if (modo == ALTERAR_ALARME) {      
-      tratamento_de_horario(&hora_alarme, &minuto_alarme);
-    }
-    else if (modo == ALTERAR_HORARIO) {
-      tratamento_de_horario(&hora, &minuto);
-    }
+}
+
+// FUNÇÕES AUXILIARES
+void incrementar_horario() {
+  hora_tmp = minuto_tmp = 0;
+  if (cursor == SECAO_HORA) hora_tmp++;
+  else minuto_tmp++;
+  if (modo == ALTERAR_ALARME) {      
+    tratamento_de_horario(&hora_alarme, &minuto_alarme);
+  }
+  else if (modo == ALTERAR_HORARIO) {
+    tratamento_de_horario(&hora, &minuto);
   }
 }
 
@@ -199,8 +209,9 @@ void blink_cursor() {
 }
 
 void acionar_alarme() {
-  set_bit(PIND, PD3);
+  PORTD |= (1 << PD3);
 }
+
 void desacionar_alarme() {
-  clear_bit(PIND, PD3);
+  PORTD &= ~(1 << PD3);
 }
